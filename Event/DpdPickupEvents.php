c08740271d47d95059322f951a6adce3ff7fc699
guigit <gbarral@openstudio.fr>
2017-01-11 09:53:33 +0100

    do not use prices.json anymore. use dpdpickup_price table instead (evolution because this json is deleted after each new version with composer install)

---
+++ Event/DpdPickupEvents.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guigit
+ * Date: 11/01/2017
+ * Time: 09:22
+ */
+
+namespace DpdPickup\Event;
+
+class DpdPickupEvents
+{
+    const DPDPICKUP_PRICE_CREATE = 'action.module.dpdpickup.price.create';
+    const DPDPICKUP_PRICE_UPDATE = 'action.module.dpdpickup.price.update';
+    const DPDPICKUP_PRICE_DELETE = 'action.module.dpdpickup.price.delete';
+}
