fdcf3bdb4ba5d9849c0f9e58392a1ddb7a04689f
Théophile Pumain <tpumain@openstudio.fr>
2017-09-15 14:25:45 +0200

    Support DPD Return service

--- Controller/ExportExaprint.php
+++ Controller/ExportExaprint.php
@@ -25,10 +25,12 @@ namespace DpdPickup\Controller;
 
 use DpdPickup\Form\ExportExaprintForm;
 use DpdPickup\DpdPickup;
+use DpdPickup\Form\ExportExaprintFormReturn;
 use Thelia\Controller\Admin\BaseAdminController;
 use Thelia\Core\Translation\Translator;
 use Thelia\Core\Security\Resource\AdminResources;
 use Thelia\Core\Security\AccessManager;
+use Thelia\Model\Exception\InvalidArgumentException;
 
 /**
  * Class ExportExaprint
@@ -52,15 +54,34 @@ class ExportExaprint extends BaseAdminController
         try {
             $vform = $this->validateForm($form);
 
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_NAME, $vform->get('name')->getData());
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_ADDR, $vform->get('addr')->getData());
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_ADDR2, $vform->get('addr2')->getData());
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_ZIPCODE, $vform->get('zipcode')->getData());
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_CITY, $vform->get('city')->getData());
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_TEL, $vform->get('tel')->getData());
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_MOBILE, $vform->get('mobile')->getData());
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_MAIL, $vform->get('mail')->getData());
-            DpdPickup::setConfigValue(DpdPickup::CONF_EXA_EXPCODE, $vform->get('expcode')->getData());
+            $return_type = $vform->get('return_type')->getData();
+
+            if (!$this->isCorrectReturnType($return_type)) {
+                throw new InvalidArgumentException("Incorrect return code.");
+            }
+
+            if ($return_type == DpdPickup::RETURN_ON_DEMAND || $return_type == DpdPickup::RETURN_PREPARED) {
+                $this->checkFormReturnFields($vform);
+                DpdPickup::setConfigValue(DpdPickup::KEY_RETURN_NAME, $vform->get('return_name')->getData());
+                DpdPickup::setConfigValue(DpdPickup::KEY_RETURN_ADDR, $vform->get('return_addr')->getData());
+                DpdPickup::setConfigValue(DpdPickup::KEY_RETURN_ADDR2, $vform->get('return_addr2')->getData());
+                DpdPickup::setConfigValue(DpdPickup::KEY_RETURN_ZIPCODE, $vform->get('return_zipcode')->getData());
+                DpdPickup::setConfigValue(DpdPickup::KEY_RETURN_CITY, $vform->get('return_city')->getData());
+                DpdPickup::setConfigValue(DpdPickup::KEY_RETURN_TEL, $vform->get('return_tel')->getData());
+            }
+
+            DpdPickup::setConfigValue(DpdPickup::KEY_RETURN_TYPE, $return_type);
+
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_NAME, $vform->get('exp_name')->getData());
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_ADDR, $vform->get('exp_addr')->getData());
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_ADDR2, $vform->get('exp_addr2')->getData());
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_ZIPCODE, $vform->get('exp_zipcode')->getData());
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_CITY, $vform->get('exp_city')->getData());
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_TEL, $vform->get('exp_tel')->getData());
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_MOBILE, $vform->get('exp_mobile')->getData());
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_MAIL, $vform->get('exp_mail')->getData());
+            DpdPickup::setConfigValue(DpdPickup::KEY_EXPEDITOR_DPDCODE, $vform->get('exp_dpdcode')->getData());
+
 
             return $this->generateRedirectFromRoute(
                 "admin.module.configure",
@@ -88,5 +109,28 @@ class ExportExaprint extends BaseAdminController
                 'current_tab' => "configure_export_exaprint"
             ]
         );
+    }
+
+    public function isCorrectReturnType($returnType)
+    {
+        return $returnType == DpdPickup::RETURN_NONE
+            || $returnType == DpdPickup::RETURN_ON_DEMAND
+            || $returnType == DpdPickup::RETURN_PREPARED;
+    }
+
+    /**
+     * @param $vform \Symfony\Component\Form\Form
+     */
+    public function checkFormReturnFields($vform)
+    {
+        $requiredKeys = ['return_name', 'return_addr', 'return_zipcode', 'return_city', 'return_tel'];
+
+        foreach ($requiredKeys as $key) {
+            $field = $vform->get($key);
+            if (empty($field->getData())) {
+                $translatedFieldName = $this->getTranslator()->trans($field->getName(), [], DpdPickup::DOMAIN_ADMIN);
+                throw new InvalidArgumentException($this->getTranslator()->trans("Empty value not allowed", [], DpdPickup::DOMAIN)." : ".$translatedFieldName);
+            }
+        }
     }
 }
