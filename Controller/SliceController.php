8f51142658555498f8a4fc2e0fff240bdc9a3144
Alexandre Bothamy <bothamy.alexandre@yahoo.fr>
2018-04-23 17:09:56 +0200

    Handle actions on prices

--- Controller/SliceController.php
+++ Controller/SliceController.php
@@ -2,10 +2,9 @@
 
 namespace DpdPickup\Controller;
 
-use Propel\Runtime\Map\TableMap;
+use DpdPickup\DpdPickup;
 use DpdPickup\Model\DpdpickupPrice;
 use DpdPickup\Model\DpdpickupPriceQuery;
-use DpdPickup\DpdPickup;
 use Thelia\Controller\Admin\BaseAdminController;
 use Thelia\Core\Security\AccessManager;
 
@@ -13,38 +12,34 @@ class SliceController extends BaseAdminController
 {
     public function deleteSliceAction()
     {
-        $response = $this->checkAuth([], ['DpdPickup'], AccessManager::DELETE);
-
-        if (null !== $response) {
+        if (null !== $response =
+                $this->checkAuth(
+                    [],
+                    ['DpdPickup'],
+                    [AccessManager::DELETE]
+                )
+        ) {
             return $response;
         }
-
-        $this->checkXmlHttpRequest();
-
-        $responseData = [
-            "success" => false,
-            "message" => '',
-            "slice" => null
-        ];
-        $response = null;
+        $message = [];
 
         try {
-            $requestData = $this->getRequest()->request;
-
-            if (0 !== $id = intval($requestData->get('id'))) {
+            if (0 !== $id = intval($this->getRequest()->request->get('id'))) {
                 $slice = DpdpickupPriceQuery::create()->findPk($id);
-                $slice->delete();
-                $responseData['success'] = true;
+                if (null !== $slice) {
+                    $slice->delete();
+                }
             } else {
-                $responseData['message'] = $this->getTranslator()->trans(
+                $message[] = $this->getTranslator()->trans(
                     'The slice has not been deleted',
                     [],
                     dpdpickup::DOMAIN
                 );
             }
         } catch (\Exception $e) {
-            $responseData['message'] = $e->getMessage();
+            $message[] = $e->getMessage();
         }
+
         return $this->generateRedirectFromRoute(
             "admin.module.configure",
             [],
@@ -52,46 +47,36 @@ class SliceController extends BaseAdminController
                 'module_code'=>"DpdPickup",
                 'current_tab'=>"price_slices_tab",
                 '_controller' => 'Thelia\\Controller\\Admin\\ModuleController::configureAction'
-            ]);
+            ]
+        );
     }
 
     public function saveSliceAction()
     {
-        $response = $this->checkAuth([], ['DpdPickup'], AccessManager::UPDATE);
-
-        if (null !== $response) {
+        if (null !== $response =
+                $this->checkAuth(
+                    [],
+                    ['DpdClassic'],
+                    [AccessManager::UPDATE]
+                )
+        ) {
             return $response;
         }
-
-        $this->checkXmlHttpRequest();
-
-        $responseData = [
-            "success" => false,
-            "message" => '',
-            "slice" => null
-        ];
-
-        $messages = [];
-        $response = null;
+        $message = [];
 
         try {
             $requestData = $this->getRequest()->request;
-            if (0 !== $id = intval($requestData->get('id', 0)))
-            {
+
+            if ((0 !== $id = intval($requestData->get('id', 0)))) {
                 $slice = DpdpickupPriceQuery::create()->findPk($id);
-            }
-            else
-            {
+            } else {
                 $slice = new DpdpickupPrice();
             }
 
-            if (0 !== $areaId = intval($requestData->get('area', 0)))
-            {
+            if (0 !== $areaId = intval($requestData->get('area', 0))) {
                 $slice->setAreaId($areaId);
-            }
-            else
-            {
-                $messages[] = $this->getTranslator()->trans(
+            } else {
+                $message[] = $this->getTranslator()->trans(
                     'The area is not valid',
                     [],
                     dpdpickup::DOMAIN
@@ -102,10 +87,11 @@ class SliceController extends BaseAdminController
 
             if (!empty($requestWeight)) {
                 $weight= $this->getFloatVal($requestWeight);
+
                 if ((0 < $weight) || ( $requestWeight != $weight)) {
                     $slice->setWeight($weight);
                 } else {
-                    $messages[] = $this->getTranslator()->trans(
+                    $message[] = $this->getTranslator()->trans(
                         'The weight value is not valid',
                         [],
                         dpdpickup::DOMAIN
@@ -119,30 +105,24 @@ class SliceController extends BaseAdminController
             if (0 <= $price) {
                 $slice->setPrice($price);
             } else {
-                $messages[] = $this->getTranslator()->trans(
+                $message[] = $this->getTranslator()->trans(
                     'The price value is not valid',
                     [],
                     dpdpickup::DOMAIN
                 );
             }
 
-            if (0 === count($messages)) {
+            if (0 === count($message)) {
                 $slice->save();
-                $messages[] = $this->getTranslator()->trans(
+                $message[] = $this->getTranslator()->trans(
                     'Your slice has been saved',
                     [],
                     dpdpickup::DOMAIN
                 );
-
-                $responseData['success'] = true;
-                $responseData['slice'] = $slice->toArray(TableMap::TYPE_STUDLYPHPNAME);
             }
         } catch (\Exception $e) {
             $messages[] = $e->getMessage();
         }
-
-        $responseData['message'] = $messages;
-
 
         return $this->generateRedirectFromRoute(
             "admin.module.configure",
@@ -151,12 +131,15 @@ class SliceController extends BaseAdminController
                 'module_code'=>"DpdPickup",
                 'current_tab'=>"price_slices_tab",
                 '_controller' => 'Thelia\\Controller\\Admin\\ModuleController::configureAction'
-            ]);
+            ]
+        );
     }
+
     protected function getFloatVal($val, $default = -1)
     {
         if (preg_match("#^([0-9\.,]+)$#", $val, $match)) {
             $val = $match[0];
+
             if (strstr($val, ",")) {
                 $val = str_replace(".", "", $val);
                 $val = str_replace(",", ".", $val);
@@ -165,7 +148,6 @@ class SliceController extends BaseAdminController
 
             return $val;
         }
-
         return $default;
     }
 }
