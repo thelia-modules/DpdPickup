55e712d9e92f99fdb62bec9e4f7fde3df9beb844
Alexandre <anoziere@openstudio.fr>
2025-01-24 15:47:16 +0100

    feat: compliance new API thelia 2.6 + refacto + add soap dependencie

--- Listener/APIListener.php
+++ Listener/APIListener.php
@@ -1,45 +1,43 @@
 <?php
 
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace DpdPickup\Listener;
 
-
 use DpdPickup\DpdPickup;
-use OpenApi\Events\DeliveryModuleOptionEvent;
-use OpenApi\Events\OpenApiEvents;
-use OpenApi\Model\Api\DeliveryModuleOption;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Thelia\Api\Bridge\Propel\Event\DeliveryModuleOptionEvent;
+use Thelia\Api\Resource\DeliveryModuleOption;
 use Thelia\Core\Event\Delivery\PickupLocationEvent;
 use Thelia\Core\Event\TheliaEvents;
+use Thelia\Core\HttpFoundation\Session\Session;
 use Thelia\Log\Tlog;
 use Thelia\Model\PickupLocation;
 use Thelia\Model\PickupLocationAddress;
 
 class APIListener implements EventSubscriberInterface
 {
-    /** @var Container */
-    private $container;
-
-    /** @var RequestStack */
-    private $requestStack;
-
-    public function __construct(ContainerInterface $container, RequestStack $requestStack)
-    {
-        $this->container = $container;
-        $this->requestStack = $requestStack;
+    public function __construct(
+        private ContainerInterface $container,
+        private Session $session
+    ) {
     }
 
     /**
-     * Calls the Chronopost API and returns a response containing the informations of the relay points found
+     * Calls the Chronopost API and returns a response containing the informations of the relay points found.
      *
-     * @param PickupLocationEvent $pickupLocationEvent
-     * @return mixed
      * @throws \ErrorException
      */
-    protected function callWebService(PickupLocationEvent $pickupLocationEvent)
+    protected function callWebService(PickupLocationEvent $pickupLocationEvent): ?\SimpleXMLElement
     {
         $excludeZipCodes = DpdPickup::getConfigExcludeZipCode();
 
@@ -49,28 +47,27 @@ class APIListener implements EventSubscriberInterface
         $date = date('d/m/Y');
 
         try {
-            $getPudoSoap = new \SoapClient(__DIR__ . "/../Config/exapaq.wsdl", array('soap_version' => SOAP_1_2));
+            $getPudoSoap = new \SoapClient(__DIR__.'/../Config/exapaq.wsdl', ['soap_version' => SOAP_1_2]);
 
-            if (in_array($zipCode, $excludeZipCodes)) {
+            if (\in_array($zipCode, $excludeZipCodes)) {
                 return null;
             }
 
             $responses = $getPudoSoap->GetPudoList(
-                array(
-                    "address"    => str_replace(" ", "%", $address),
-                    "zipCode"    => $zipCode,
-                    "city"       => str_replace(" ", "%", $city),
-                    "request_id" => "1234",
-                    "date_from"  => $date
-                )
+                [
+                    'address' => str_replace(' ', '%', $address),
+                    'zipCode' => $zipCode,
+                    'city' => str_replace(' ', '%', $city),
+                    'request_id' => '1234',
+                    'date_from' => $date,
+                ]
             );
-
         } catch (\SoapFault $e) {
             Tlog::getInstance()->error(
                 sprintf(
-                    "[%s %s - SOAP Error %d]: %s",
+                    '[%s %s - SOAP Error %d]: %s',
                     $date,
-                    date("H:i:s"),
+                    date('H:i:s'),
                     (int) $e->getCode(),
                     (string) $e->getMessage()
                 )
@@ -81,40 +78,40 @@ class APIListener implements EventSubscriberInterface
 
         $xml = new \SimpleXMLElement($responses->GetPudoListResult->any);
         if (isset($xml->ERROR)) {
-            throw new \ErrorException("Error while choosing pick-up & go store: " . $xml->ERROR);
+            throw new \ErrorException('Error while choosing pick-up & go store: '.$xml->ERROR);
         }
 
         return $xml->PUDO_ITEMS;
     }
 
-    public function getDeliveryModuleOptions(DeliveryModuleOptionEvent $deliveryModuleOptionEvent)
+    public function getDeliveryModuleOptions(DeliveryModuleOptionEvent $deliveryModuleOptionEvent): void
     {
         if ($deliveryModuleOptionEvent->getModule()->getId() !== DpdPickup::getModuleId()) {
-            return ;
+            return;
         }
 
         $isValid = true;
-        $locale = $this->requestStack->getCurrentRequest()->getSession()->getLang()->getLocale();
+        $locale = $this->session->getLang()->getLocale();
         $orderPostage = null;
 
         try {
             $module = new DpdPickup();
             $country = $deliveryModuleOptionEvent->getCountry();
-
+            $cart = $deliveryModuleOptionEvent->getCart();
+            if (null === $cart) {
+                throw new \Exception('Cart not found');
+            }
             $orderPostage = $module->getOrderPostage(
                 $country,
-                $deliveryModuleOptionEvent->getCart()->getWeight(),
+                $cart->getWeight(),
                 $locale,
-                $deliveryModuleOptionEvent->getCart()->getTaxedAmount($country)
+                $cart->getTaxedAmount($country)
             );
-
         } catch (\Exception $exception) {
             $isValid = false;
         }
 
-
-        /** @var DeliveryModuleOption $deliveryModuleOption */
-        $deliveryModuleOption = ($this->container->get('open_api.model.factory'))->buildModel('DeliveryModuleOption');
+        $deliveryModuleOption = new DeliveryModuleOption();
         $deliveryModuleOption
             ->setCode('DpdPickup')
             ->setValid($isValid)
@@ -131,29 +128,26 @@ class APIListener implements EventSubscriberInterface
     }
 
     /**
-     * Creates and returns a new location address
-     *
-     * @param $response
-     * @return PickupLocationAddress
+     * Creates and returns a new location address.
      */
-    protected function createPickupLocationAddressFromResponse($response)
+    protected function createPickupLocationAddressFromResponse(mixed $response): PickupLocationAddress
     {
         /** We create the new location address */
         $pickupLocationAddress = new PickupLocationAddress();
 
-        /** We set the differents properties of the location address */
+        /* We set the differents properties of the location address */
         $pickupLocationAddress
-            ->setId((string)$response->PUDO_ID)
-            ->setTitle((string)$response->NAME)
-            ->setAddress1((string)$response->ADDRESS1)
-            ->setAddress2((string)$response->ADDRESS2)
-            ->setAddress3((string)$response->ADDRESS3)
-            ->setCity((string)$response->CITY)
-            ->setZipCode((string)$response->ZIPCODE)
+            ->setId((string) $response->PUDO_ID)
+            ->setTitle((string) $response->NAME)
+            ->setAddress1((string) $response->ADDRESS1)
+            ->setAddress2((string) $response->ADDRESS2)
+            ->setAddress3((string) $response->ADDRESS3)
+            ->setCity((string) $response->CITY)
+            ->setZipCode((string) $response->ZIPCODE)
             ->setPhoneNumber('')
             ->setCellphoneNumber('')
             ->setCompany('')
-            ->setCountryCode('FR') /** DPD Pickup only delivers in France as of 23/06/2020 */
+            ->setCountryCode('FR') /* DPD Pickup only delivers in France as of 23/06/2020 */
             ->setFirstName('')
             ->setLastName('')
             ->setIsDefault(0)
@@ -165,55 +159,50 @@ class APIListener implements EventSubscriberInterface
     }
 
     /**
-     * Creates then returns a location from a response of the WebService
+     * Creates then returns a location from a response of the WebService.
      *
-     * @param $response
-     * @return PickupLocation
      * @throws \Exception
      */
-    protected function createPickupLocationFromResponse($response)
+    protected function createPickupLocationFromResponse($response): PickupLocation
     {
         /** We create the new location */
         $pickupLocation = new PickupLocation();
 
-        /** We set the differents properties of the location */
+        /* We set the differents properties of the location */
         $pickupLocation
-            ->setId((string)$response->PUDO_ID)
-            ->setTitle((string)$response->NAME)
+            ->setId((string) $response->PUDO_ID)
+            ->setTitle((string) $response->NAME)
             ->setAddress($this->createPickupLocationAddressFromResponse($response))
-            ->setLatitude(str_replace(',', '.', (string)$response->LATITUDE))
-            ->setLongitude(str_replace(',', '.', (string)$response->LONGITUDE))
+            ->setLatitude(str_replace(',', '.', (string) $response->LATITUDE))
+            ->setLongitude(str_replace(',', '.', (string) $response->LONGITUDE))
             ->setModuleId(DpdPickup::getModuleId())
         ;
 
-        /** We set the opening hours separately since we got them as an array */
+        /* We set the opening hours separately since we got them as an array */
         foreach ($response->OPENING_HOURS_ITEMS->OPENING_HOURS_ITEM as $horaire) {
-            $openedHours = $pickupLocation->getOpeningHours()[($horaire->DAY_ID - 1)];
-            $openedHours .= $openedHours === null ? $horaire->START_TM . '-' . $horaire->END_TM : ' ' . $horaire->START_TM . '-' . $horaire->END_TM;
-            $pickupLocation->setOpeningHours(($horaire->DAY_ID - 1), $openedHours);
+            $openedHours = $pickupLocation->getOpeningHours()[$horaire->DAY_ID - 1];
+            $openedHours .= $openedHours === null ? $horaire->START_TM.'-'.$horaire->END_TM : ' '.$horaire->START_TM.'-'.$horaire->END_TM;
+            $pickupLocation->setOpeningHours($horaire->DAY_ID - 1, $openedHours);
         }
 
         return $pickupLocation;
     }
 
     /**
-     * Get the list of locations (relay points)
+     * Get the list of locations (relay points).
      *
-     * @param PickupLocationEvent $pickupLocationEvent
      * @throws \Exception
      */
-    public function getPickupLocations(PickupLocationEvent $pickupLocationEvent)
+    public function getPickupLocations(PickupLocationEvent $pickupLocationEvent): void
     {
-        if (null !== $moduleIds = $pickupLocationEvent->getModuleIds()) {
-            if (!in_array(DpdPickup::getModuleId(), $moduleIds)) {
-                return ;
-            }
+        if ((null !== $moduleIds = $pickupLocationEvent->getModuleIds()) && !\in_array(DpdPickup::getModuleId(), $moduleIds, true)) {
+            return;
         }
 
         $responses = $this->callWebService($pickupLocationEvent);
 
         if (null === $responses) {
-            return ;
+            return;
         }
 
         foreach ($responses->PUDO_ITEM as $response) {
@@ -221,17 +210,17 @@ class APIListener implements EventSubscriberInterface
         }
     }
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         $listenedEvents = [];
 
-        /** Check for old versions of Thelia where the events used by the API didn't exists */
+        /* Check for old versions of Thelia where the events used by the API didn't exists */
         if (class_exists(PickupLocation::class)) {
-            $listenedEvents[TheliaEvents::MODULE_DELIVERY_GET_PICKUP_LOCATIONS] = array("getPickupLocations", 131);
+            $listenedEvents[TheliaEvents::MODULE_DELIVERY_GET_PICKUP_LOCATIONS] = ['getPickupLocations', 131];
         }
 
         if (class_exists(DeliveryModuleOptionEvent::class)) {
-            $listenedEvents[OpenApiEvents::MODULE_DELIVERY_GET_OPTIONS] = array("getDeliveryModuleOptions", 128);
+            $listenedEvents[TheliaEvents::MODULE_DELIVERY_GET_OPTIONS] = ['getDeliveryModuleOptions', 128];
         }
 
         return $listenedEvents;
